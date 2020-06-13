b2b7a9b9dd8312636c07271a3294cedbb2fada15
Henry Oladeji <officialmonstajamss@gmail.com>
2020-06-13 16:41:14 +0100

    Login Page Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                     @if (Route::has('password.request'))
                                     <p class="message"><a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
-                                    </a></p> / <p><a href="/register">Click Here To Register</a></p>
+                                    </a></p> OR <p class="message"><a class="btn btn-link" href="/register">Click Here To Register</a></p>
                                 @endif 
                     <p class="message"><a href="/">Go back to homepage</a></p> 
                   </form>
